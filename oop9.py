1093796c8548a2205e59fe88902d326a51baf487
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-19 14:39:17 +0600

    Turtle

---
+++ oop9.py
@@ -0,0 +1,8 @@
+from turtle import Turtle,Screen
+from prettytable import PrettyTable
+jj = Turtle()
+jj.shape("turtle")
+jj.color("red")
+jj.forward(200)
+myscreen = Screen()
+myscreen.exitonclick()
